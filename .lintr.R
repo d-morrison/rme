6ad6c77cf0914a8ea278268adae6ebf13a0f514f
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2025-04-24 16:18:16 -0700

    fix lints

--- .lintr.R
+++ .lintr.R
@@ -1,13 +1,13 @@
 # define snake_case with uppercase acronyms allowed;
 # see https://github.com/r-lib/lintr/issues/2844 for details:
 withr::local_package("rex")
-snake_case_ACRO <- rex::rex(
+snake_case_ACROs <- rex::rex(
   start,
   maybe("."),
-  some_of(lower, digit) %or% some_of(upper, digit),
+  some_of(lower, upper, digit),
   zero_or_more(
     "_",
-    some_of(lower, digit) %or% some_of(upper, digit)
+    some_of(lower, upper, digit)
   ),
   end
 )
@@ -17,12 +17,12 @@ linters <- lintr::linters_with_defaults(
   trailing_whitespace_linter = NULL,
   lintr::pipe_consistency_linter(pipe = "|>"),
   lintr::object_name_linter(
-    regexes = c(snake_case_ACRO = snake_case_ACRO)
+    regexes = c(snake_case_ACROs = snake_case_ACROs)
   )
 )
 
 # prevent warnings from lintr::read_settings:
-rm(snake_case_ACRO)
+rm(snake_case_ACROs)
 exclusions <- list(
   `data-raw` = list(
     pipe_consistency_linter = Inf
